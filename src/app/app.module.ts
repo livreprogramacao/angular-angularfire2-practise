434e2fe753c58de6d2d7edf5a646d55b4f3e966b
Livre programacao ltda-me <livre.programacao@gmail.com>
2017-08-03 09:53:50 -0300

    Assunto ( subject line ) Setup @NgModule for the AngularFireModule
    Descrição ( what happened )
    Open /src/app/app.module.ts, inject the Firebase providers, and specify your Firebase configuration. This can be found in your project at the Firebase Console.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'app-name'),
     FormsModule,
     HttpModule,
     AppRoutingModule
