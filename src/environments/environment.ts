c4982fd84bdad3ba7366fc6525670cea4c323635
Livre programacao ltda-me <livre.programacao@gmail.com>
2017-08-03 09:47:07 -0300

    Assunto ( subject line ) Add Firebase config to environments variable
    Descrição ( what happened )
    Open /src/environments/environment.ts and add your Firebase configuration.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBP0Mt0qxEoxGffv4TgnwqggWHzJc0bWYc',
+    authDomain: 'auth-f9004.firebaseapp.com',
+    databaseURL: 'https://auth-f9004.firebaseio.com',
+    projectId: 'auth-f9004',
+    storageBucket: 'auth-f9004.appspot.com',
+    messagingSenderId: '215583315022'
+  }
 };
