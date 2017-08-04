177994836aa0b529698ac1342dc5f30d846593c3
Livre programacao ltda-me <livre.programacao@gmail.com>
2017-08-04 09:08:47 -0300

    Assunto ( subject line ) User Authentication - FirebaseAuthentication.
    Descrição ( what happened )
    AngularFireAuth.authState provides you an Observable<firebase.User> to monitor your application's authentication State.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +12,22 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   items: FirebaseListObservable<any[]>;
-  constructor(db: AngularFireDatabase) {
+  user: Observable<firebase.User>;
+
+  constructor(db: AngularFireDatabase, public afAuth: AngularFireAuth) {
     this.items = db.list('/items');
+    this.user = afAuth.authState;
   }
-  title = 'app works!';
+  title = 'app Using AngularFire2 works!';
+
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
